2e23322f291aa8ccd117e126f0502edf6286186e
myKim <myung120@gmail.com>
2021-01-07 23:09:55 +0900

    create initial sample project

---
+++ objective-c-example/objective-c-example/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  objective-c-example
+//
+//  Created by 김명유 on 2021/01/07.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
